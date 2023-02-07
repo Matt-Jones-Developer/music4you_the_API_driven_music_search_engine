5d5ad04fd604fdf06ba9b24bfe0915e10d0f3c0c
Matt Jones <glitchyghost81@protonmail.com>
2023-02-07 14:39:30 +0000

    added a config file for APIs

---
+++ assets/scripts/config.js
@@ -0,0 +1,30 @@
+// our API keys are hidden via .gitignore 
+
+// all API keys and secrets
+
+// discogs key and secret
+const apiKeyCogs = "SUabynzSHsiRIoQDRiOv"
+const secret = "YVLXccThJwokGpRwpgaQzkvUJKSlkhun"
+
+// lastFM keys
+const apiKeyLast = "a87d07d178f9511fa2e22a54dbe7f678"
+
+// musixmatch api
+const apiKeyMusix = '418ba039b53e2d2e88ebda069a30a55e'
+
+// wikiMedia keys
+
+// Client ID:
+// 40736fd6fadb2d9f4563d29b105788eb
+// Client secret:
+// 9753939a04553de256b898ae5bf07485e01acd94
+
+const apiIDWiki = '40736fd6fadb2d9f4563d29b105788eb'
+const secretWiki = '9753939a04553de256b898ae5bf07485e01acd94'
+
+
+// ticketmaster Api key
+const ticketMasterAPIKey = 'GbIS7ZHqNdNaR8p53t0s3R87G5IbJWSB'
+
+
+
